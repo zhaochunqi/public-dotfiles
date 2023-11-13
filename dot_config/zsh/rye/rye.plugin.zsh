0cfc561f0236e430ad5640b04937ff46c99d1214
Alex Zhao <git@zhaochunqi.com>
2023-11-14 00:01:48 +0800

    remove some unused config

--- dot_config/zsh/rye/rye.plugin.zsh
+++
@@ -1,5 +0,0 @@
-if command -v rye>/dev/null 2>&1; then
-  # rye
-  source "$HOME/.rye/env"
-fi
-
--- dot_config/zsh/volta/volta.plugin.zsh
+++
@@ -1,4 +0,0 @@
-if [[ -s "${HOME}/.volta/bin/volta" ]]; then
-  export VOLTA_HOME="$HOME/.volta"
-  export PATH="$VOLTA_HOME/bin:$PATH"
-fi
