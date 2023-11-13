8103a4ff61433e5c89a7f6b292150dda5d8176b3
zhaochunqi <zcq.qiqi@gmail.com>
2023-11-13 15:46:11 +0000

    update files 2023-11-13

--- dot_config/zsh/asdf/asdf.plugin.zsh
+++ dot_config/zsh/asdf/asdf.plugin.zsh
@@ -11,3 +11,14 @@ if [[ -f "$ASDF_DIR/asdf.sh" ]]; then
     . "$ASDF_COMPLETIONS/asdf.bash"
   fi
 fi
+
+# ASDF CONFIGS
+export ASDF_DATA_DIR=~/.asdf
+java_home="~/.asdf/plugins/java/set-java-home.zsh"
+if [ -f "$java_home" ]; then
+  . $java_home
+fi
+golang_env="~/.asdf/plugins/golang/set-env.zsh"
+if [ -f "golang_env" ]; then
+  . $golang_env
+fi
