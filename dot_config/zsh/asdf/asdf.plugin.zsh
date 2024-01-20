d88522ddda8e6b9d26da2188eae900e1eae06959
zhaochunqi <zcq.qiqi@gmail.com>
2024-01-20 17:37:46 +0000

    update files 2024-01-20

--- dot_config/zsh/asdf/asdf.plugin.zsh
+++ dot_config/zsh/asdf/asdf.plugin.zsh
@@ -18,6 +18,7 @@ java_home="~/.asdf/plugins/java/set-java-home.zsh"
 if [ -f "$java_home" ]; then
   . $java_home
 fi
+
 golang_env="~/.asdf/plugins/golang/set-env.zsh"
 if [ -f "golang_env" ]; then
   . $golang_env
