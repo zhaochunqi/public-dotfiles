0607964697d49fa561819dde0a6b38f029173824
zhaochunqi <zcq.qiqi@gmail.com>
2024-06-10 09:21:01 +0000

    update files 2024-06-10

--- dot_config/zsh/mise/mise.plugin.zsh
+++ dot_config/zsh/mise/mise.plugin.zsh
@@ -1,14 +1,3 @@
 # Load command
 eval "$(mise activate zsh)"
 
-# ASDF CONFIGS
-#export ASDF_DATA_DIR=~/.asdf
-#java_home=~/.asdf/plugins/java/set-java-home.zsh
-#if [ -f "$java_home" ]; then
-#  . $java_home
-#fi
-
-#golang_env=~/.asdf/plugins/golang/set-env.zsh
-#if [ -f "$golang_env" ]; then
-#  . $golang_env
-#fi
