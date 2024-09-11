3a2f1b8b170732f7b81ebe4fc96c7448c99cc297
zhaochunqi <zcq.qiqi@gmail.com>
2024-09-11 05:43:47 +0000

    update files 2024-09-11

--- dot_config/zsh/mise/mise.plugin.zsh
+++ dot_config/zsh/mise/mise.plugin.zsh
@@ -2,3 +2,8 @@
 if command -v mise &> /dev/null; then
     eval "$(mise activate zsh)"
 fi
+
+# Check if mise and flutter exist, then export FLUTTER_ROOT
+if command -v mise &> /dev/null && command -v flutter &> /dev/null; then
+    export FLUTTER_ROOT="$(mise where flutter)"
+fi
